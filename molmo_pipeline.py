a4a4b45a351a2a68fec3f6fab9b3d0f0510f7b91
velythyl <charlie.gauthier@umontreal.ca>
2024-10-24 14:38:12 -0400

    dang

--- molmo_pipeline.py
+++ molmo_pipeline.py
@@ -7,3 +7,17 @@ chat = [
 
 pipe = pipeline("text-generation", "allenai/Molmo-7B-D-0924", torch_dtype=torch.bfloat16, device_map="auto")
 response = pipe(chat, max_new_tokens=512)
+
+print(response[0]['generated_text'][-1]['content'])
+
+
+print("PRINTING RESPONSE OBJECT")
+print(response)
+print("DONE")
+
+chat = response[0]['generated_text']
+chat.append(
+    {"role": "user", "content": "Wait, what's so wild about soup cans?"}
+)
+response = pipe(chat, max_new_tokens=512)
+print(response[0]['generated_text'][-1]['content'])
