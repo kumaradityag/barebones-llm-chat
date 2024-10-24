f845a5679f208c0766cfbceaa78a3107ce70e9a4
Velythyl <charlie.gauthier@umontreal.ca>
2024-10-24 17:10:20 -0400

    yea

--- molmo_chat.py
+++ molmo_chat.py
@@ -51,7 +51,7 @@ class Olmo:
 
 olmo = Olmo()    
 
-chat = ChatHistory().add(CHAT_ROLE.USER, "Describe the image").add_img(Image.open("./block-tower.jpg"))
+chat = ChatHistory().add(CHAT_ROLE.USER, "Ignore the image. My friend is disorganized, please suggest her a halloween costume!").add_img(Image.open("./block-tower.jpg"))
 
 chat = olmo.respond(chat)
 
